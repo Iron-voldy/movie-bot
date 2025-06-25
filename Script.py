34146503d7b28661e7814e39c9a3ad9396ecee5d
Iron-voldy <hasindutw,@gmail.com>
2025-06-26 01:41:10 +0530

    first commit

---
+++ Script.py
@@ -0,0 +1,187 @@
+class script(object):
+    START_TXT = """𝙷𝚎𝚕𝚕𝚊𝚘𝚠 <b>{}</b>, 
+𝙸𝚖 𝚈𝚘𝚞𝚛 𝙵𝚛𝚒𝚎𝚗𝚍𝚕𝚢 <a href=https://t.me/{}>{}</a>.
+    
+- 𝚈𝚘𝚞 𝙲𝚊𝚗 𝙶𝚎𝚝 𝚈𝚘𝚞𝚛 𝙵𝚒𝚕𝚖 𝙰𝚗𝚍 𝚂𝚎𝚛𝚒𝚎𝚜 𝙵𝚒𝚕𝚎𝚜 𝚄𝚜𝚒𝚗𝚐 𝚝𝚑𝚒𝚜 𝚋𝚘𝚝. 
+- 𝙰𝚍𝚍 𝚋𝚘𝚝 𝚝𝚘 𝚈𝚘𝚞𝚛 𝙶𝚛𝚘𝚞𝚙 𝚘𝚛 𝚁𝚎𝚚𝚞𝚎𝚜𝚝 𝙷𝚎𝚛𝚎. 😍"""
+
+    
+    HELP_TXT = """Hello {}!
+    
+Here αre тнe αvαιlαвle coммαɴdѕ.
+    
+I’м нere тo αѕѕιѕт yoυ! Feel ғree тo αѕĸ ғor αɴy ɢυιdαɴce or coммαɴdѕ yoυ мαy ɴeed.
+Leт’ѕ мαĸe тнιɴɢѕ eαѕιer ғor yoυ!"""
+
+
+    ABOUT_TXT = """Here αre ѕoмe deтαιlѕ yoυ ɴeed тo ĸɴow.
+
+✯ 𝙱𝙾𝚃 𝚃𝚈𝙿𝙴: 𝙰𝚄𝚃𝙾𝙵𝙸𝙻𝚃𝙴𝚁 𝚅𝟹	
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href="https://t.me/ImSahanSBot">𝚂𝚊𝚑𝚊𝚗𝚂</a>
+✯ 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁: <a href="https://t.me/Hansaka_Anuhas">𝙷𝚊𝚗𝚜𝚊𝚔𝚊</a> 
+✯ 𝚂𝙾𝚄𝚁𝙲𝙴: 𝙿𝚛𝚒𝚟𝚊𝚝𝚎 (ᴅᴍ ᴅᴇᴠᴇʟᴏᴘᴇʀ) """  
+
+      
+    MANUELFILTER_TXT = """Help: <b>Filters</b>
+- Fιlтerѕ αllow υѕerѕ тo ѕeт αυтoмαтed replιeѕ ғor ѕpecιғιc ĸeywordѕ, αɴd тнe вoт wιll reѕpoɴd wнeɴever α ĸeyword ιѕ ғoυɴd ιɴ тнe мeѕѕαɢe.
+
+<b>NOTE:</b>
+1. Tнιѕ вoт ѕнoυld нαve αdмιɴ prιvιleɢeѕ тo ғυɴcтιoɴ properly.  
+2. Oɴly αdмιɴѕ cαɴ αdd ғιlтerѕ ιɴ α cнαт.  
+3. Alerт вυттoɴѕ нαve α lιмιт oғ 64 cнαrαcтerѕ.
+
+
+<b>Coммαɴdѕ αɴd Uѕαɢe:</b>
+/filter - <code>Add a filter in a chat.</code>  
+/filters - <code>List all the filters in a chat.</code>  
+/del - <code>Delete a specific filter in a chat.</code>  
+/delall - <code>Delete all filters in a chat (chat owner only).</code>  """
+
+
+    BUTTON_TXT = """Help: <b>Buttons</b>
+- Tнe вoт ѕυpporтѕ вoтн URL αɴd αlerт ιɴlιɴe вυттoɴѕ.
+
+<b>NOTE:</b>
+1. Teleɢrαм wιll ɴoт αllow yoυ тo ѕeɴd вυттoɴѕ wιтнoυт αɴy coɴтeɴт, ѕo coɴтeɴт ιѕ мαɴdαтory.  
+2. Tнe вoт ѕυpporтѕ вυттoɴѕ wιтн αɴy тype oғ Teleɢrαм мedια.  
+3. Bυттoɴѕ ѕнoυld вe properly pαrѕed ιɴ мαrĸdowɴ ғorмαт.
+
+<b>URL Buttons:</b>
+<code>[Button Text](buttonurl:https://t.me/SECLK)</code>
+
+<b>Alert Buttons:</b>
+<code>[Button Text](buttonalert:This is an alert message, You should use @NETFLIXLKBOT to get Movies)</code>"""
+
+
+    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
+
+<b>NOTE:</b>
+1. Mαĸe мe тнe αdмιɴ oғ yoυr cнαɴɴel ιғ ιт'ѕ prιvαтe.  
+2. Mαĸe ѕυre тнαт yoυr cнαɴɴel doeѕ ɴoт coɴтαιɴ cαмrιpѕ, porɴ, or ғαĸe ғιleѕ.  
+3. Forwαrd тнe lαѕт мeѕѕαɢe тo мe wιтн qυoтeѕ.
+
+I’ll αdd αll тнe ғιleѕ ιɴ тнαт cнαɴɴel тo мy dαтαвαѕe."""
+
+    
+    CONNECTION_TXT = """Help: <b>Connections</b>
+
+- Uѕed тo coɴɴecт тнe вoт тo PM ғor мαɴαɢιɴɢ ғιlтerѕ.
+- Helpѕ αvoιd ѕpαммιɴɢ ιɴ ɢroυpѕ.
+
+<b>NOTE:</b>
+1. Oɴly αdмιɴѕ cαɴ αdd α coɴɴecтιoɴ.
+2. Seɴd <code>/connect</code> ғor coɴɴecтιɴɢ мe тo yoυr PM.
+
+<b>Commands and Usage:</b>
+/connect  - <code>Connect a particular chat to your PM</code>
+/disconnect  - <code>Disconnect from a chat</code>
+/connections  - <code>List all your connections</code>"""
+
+    EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
+
+<b>NOTE:</b>
+Tнeѕe αre αddιтιoɴαl ғeαтυreѕ oғ тнe Teѕѕα вoт тo eɴнαɴce yoυr eхperιeɴce.
+
+<b>Commands and Usage:</b>
+/id - <code>Retrieve the ID of a specified user.</code>
+/info - <code>Get detailed information about a user.</code>
+/imdb - <code>Fetch film information from IMDb.</code>
+/search - <code>Search for film details across multiple sources.</code>
+/ping - <code>To check bot speed.</code>
+
+Feel ғree тo υѕe тнeѕe coммαɴdѕ тo eхplore тнe вoт'ѕ cαpαвιlιтιeѕ ғυrтнer! 📚"""
+
+
+    ADMIN_TXT = """Help: <b>Admin Mods</b>
+
+<b>NOTE:</b>
+Tнιѕ мodυle ιѕ eхclυѕιvely ғor вoт αdмιɴιѕтrαтorѕ oɴly. Uѕe тнeѕe coммαɴdѕ тo мαɴαɢe υѕerѕ αɴd cнαт operαтιoɴѕ eғғecтιvely.
+
+<b>Commands and Usage:</b>
+/users - <code>Retrieve a list of all users and their IDs.</code>
+/chats - <code>Get a list of all chats and their IDs.</code>
+/leave - <code>Leave a specified chat.</code>
+/channel - <code>Get a list of all connected channels.</code>
+/broadcast - <code>Broadcast a message to all users.</code>
+
+Uѕe тнeѕe αdмιɴ coммαɴdѕ тo мαɴαɢe yoυr вoт eғғecтιvely αɴd ĸeep everyтнιɴɢ rυɴɴιɴɢ ѕмooтнly! 📊"""
+
+    
+    STATUS_TXT = """- 𝙵𝚒𝚕𝚎 𝙳𝚊𝚝𝚊𝚋𝚊𝚜𝚎 𝟷.𝟶 -
+★ ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ: <code>{}</code>
+★ ᴜꜱᴇᴅ ꜱᴛᴏʀᴀɢᴇ: <code>{}</code>
+
+- 𝙵𝚒𝚕𝚎 𝙳𝚊𝚝𝚊𝚋𝚊𝚜𝚎 𝟸.𝟶 -
+★ ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ: <code>{}</code>
+★ ᴜꜱᴇᴅ ꜱᴛᴏʀᴀɢᴇ: <code>{}</code>
+
+- 𝚄𝚜𝚎𝚛 𝙳𝚊𝚝𝚊𝚋𝚊𝚜𝚎 𝟷.𝟶 -
+★ ᴛᴏᴛᴀʟ ᴜꜱᴇʀꜱ: <code>{}</code>
+★ ᴛᴏᴛᴀʟ ᴄʜᴀᴛꜱ: <code>{}</code>
+★ ᴜꜱᴇᴅ ꜱᴛᴏʀᴀɢᴇ: <code>{}</code>
+
+- 𝚂𝚎𝚛𝚟𝚎𝚛 𝚁𝚎𝚜𝚘𝚞𝚛𝚌𝚎𝚜 -
+★ ᴛᴏᴛᴀʟ ʀᴀᴍ: <code>{}</code>
+★ ᴜsᴇᴅ ʀᴀᴍ: <code>{}</code>"""
+
+    LOG_TEXT_G = """#NewGroup
+Group = {}(<code>{}</code>)
+Total Members = <code>{}</code>
+Added By - {}
+"""
+    LOG_TEXT_P = """#NewUser
+ID - <code>{}</code>
+Name - {}
+"""
+    
+    REQINFO = """
+⚠ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ ⚠
+⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯
+ɪꜰ ʏᴏᴜ ᴅᴏ ɴᴏᴛ ꜱᴇᴇ ᴛʜᴇ ʀᴇǫᴜᴇsᴛᴇᴅ ᴍᴏᴠɪᴇ / sᴇʀɪᴇs ꜰɪʟᴇ, 
+ɢᴏ ᴛᴏ ɢᴏᴏɢʟᴇ ➠ ᴛʏᴘᴇ ᴍᴏᴠɪᴇ ᴏʀ ꜱᴇʀɪᴇꜱ ɴᴀᴍᴇ ➠ ᴄᴏᴘʏ ᴄᴏʀʀᴇᴄᴛ ɴᴀᴍᴇ ➠ ᴘᴀꜱᴛᴇ ᴛʜɪꜱ ɢʀᴏᴜᴘ"""
+
+    MINFO = """
+ᴍᴏᴠɪᴇ ʀᴇǫᴜᴇꜱᴛ ꜰᴏʀᴍᴀᴛ
+⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯
+ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴀ ᴍᴏᴠɪᴇ ғᴏʟʟᴏᴡ ᴛʜᴇ ғᴏʀᴍᴀᴛ ʙᴇʟᴏᴡ
+𝐔𝐧𝐜𝐡𝐚𝐫𝐭𝐞𝐝 | 𝐃𝐮𝐧𝐞 𝟐𝟎𝟐𝟏 | 𝐓𝐫𝐨𝐥𝐥 𝟐𝟎𝟐𝟐 𝟕𝟐𝟎𝐩
+
+🚯 ᴅᴏɴᴛ ᴜꜱᴇ ➠ ':(!,./)"""
+
+    SINFO = """
+ꜱᴇʀɪᴇꜱ ʀᴇǫᴜᴇꜱᴛ ꜰᴏʀᴍᴀᴛ
+⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯
+ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴀ sᴇʀɪᴇs ғᴏʟʟᴏᴡ ᴛʜᴇ ғᴏʀᴍᴀᴛ ʙᴇʟᴏᴡ
+𝐋𝐨𝐤𝐢 𝐒𝟎𝟏𝐄𝟎𝟏 | 𝐘𝐨𝐮 𝐒𝟎𝟑 | 𝐖𝐞𝐝𝐧𝐞𝐬𝐝𝐚𝐲 𝐒𝟎𝟏 𝟕𝟐𝟎𝐩
+
+🚯 ᴅᴏɴᴛ ᴜꜱᴇ ➠ ':(!,./)"""
+    
+    
+    OWNER_INFO = """
+<b>⍟───[ ᴏᴡɴᴇʀ ᴅᴇᴛᴀɪʟꜱ ]───⍟
+⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯    
+• ꜰᴜʟʟ ɴᴀᴍᴇ : 𝚂𝚊𝚑𝚊𝚗 𝚂𝚊𝚗𝚍𝚊𝚛𝚞𝚠𝚊𝚗
+• ᴀʙᴏᴜᴛ : <a href='https://t.me/About_Sandaruwan'>𝙰𝚋𝚘𝚞𝚝 𝚂𝚊𝚗𝚍𝚊𝚛𝚞𝚠𝚊𝚗</a>
+• ᴅᴍ ʟɪɴᴋ : <a href='https://t.me/Sandaruwan_Feedback_Bot'>𝚂𝚊𝚑𝚊𝚗𝚂</a></b>"""
+
+    IMDB_TEMPLATE = """
+🎬 <b><a href={url}>{title}</a> ({year})</b>  
+‌‌‌‌<b>{runtime}min | {release_date}</b>  
+
+‌‌‌‌<b>⭐️ IMDB</b> ➠ <b><i>{rating}/10 ({votes})</i></b>  
+‌‌‌‌<b>🌏 Country</b> ➠ <b><i>{countries}</i></b>  
+<b>🔉 Language</b> ➠ <b><i>{languages}</i></b>  
+‌‌‌‌‌‌‌‌‌‌‌‌<b>⚙️ Genres</b> ➠ <b><i>{genres}</i></b>  
+
+‌‌‌‌®️ <b><a href='https://t.me/SECL4U'>Mαιɴ Cнαɴɴel</a></b>
+"""
+
+    FILE_CAPTION = """➥ 𝗙𝗶𝗹𝗲 𝗡𝗮𝗺𝗲: <b>@SECL4U </b><code>{file_name}</code>
+
+<b><i>⚠️ Warning:</b> This file will be deleted within 5 minutes. Please make sure to forward it to your <b>“Saved Messages”</b> before downloading.</i>
+
+➠ 𝗛𝗮𝘃𝗶𝗻𝗴 𝗶𝘀𝘀𝘂𝗲: <a href='https://t.me/SECL4U/54'>𝙏𝙧𝙮 𝙖𝙣𝙤𝙩𝙝𝙚𝙧 𝙗𝙤𝙩</a>
+➠ 𝗡𝗲𝘄 𝘁𝗼 𝘁𝗵𝗶𝘀 𝗯𝗼𝘁: <a href='https://t.me/SECOfficial_Bot'>𝙒𝙖𝙩𝙘𝙝 𝙩𝙝𝙚 𝙜𝙪𝙞𝙙𝙚</a>
+━━━━━━━━━━━━━━‌‌
+🪫 𝗣𝗼𝘄𝗲𝗿𝗲𝗱 𝗯𝘆: 
+<b>- @SECLK | @CeylonCryptoSL -</b>"""
